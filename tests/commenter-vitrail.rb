42a7037a3530262b2500d7f8d4ecab18facdce34
EDOH-KOSSI <amenuve_mokpokpo.edoh_kossi@utt.fr>
2016-05-30 22:42:28 +0200

    Mis à jour du test avec le cas d'une réponse à un commentaire

--- tests/commenter-vitrail.rb
+++ tests/commenter-vitrail.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Commenter un vitrail' do
 
-  scenario 'Commenter un vitrail' do
+  scenario 'Cas de commentaire initial ' do
     visit '/'
     click_on 'Vitraux indéterminés'
     click_on 'SNZ 104'
@@ -10,6 +10,17 @@ feature 'Commenter un vitrail' do
     fill_in 'Commentaire', :with => 'mon super commentaire'
     click_on 'Envoyer'
     expect(page).to have_content 'mon super commentaire'
+  end
+  
+   scenario 'Commentaire sur un autre commentaire' do
+    visit '/'
+    click_on 'Vitraux indéterminés'
+    click_on 'SNZ 104'
+    expect(page).to have_content 'mon super commentaire'
+    click_on 'Répondre'
+    fill_in 'Commentaire', :with => 'ma réponse au super commentaire'
+    click_on 'Envoyer'
+    expect(page).to have_content 'ma réponse au super commentaire'
   end
 
 end
