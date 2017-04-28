bb14bcfd68e9d615acc69c54c1dcd9d554bd80f0
LO10 <lo10@mac02-m102.utt.fr>
2017-04-28 11:52:26 +0200

    Ajout des step_definitions et modification de deux scénarios à problème.

---
+++ Features/step_definitions/Ajouter-Modifier les tags a un vitrail_step.rb
@@ -0,0 +1,55 @@
+Etantdonnéque(/^Pierre a listé les vitraux$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Etantdonnéque(/^il souhaite ajouter un nouveau tag pour un vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^il clique sur le vitrail cible$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^clique sur le bouton "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^renseigne la description du nouveau tag$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^le nouveau tag est bien créé$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^il est bien associé au vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Etantdonnéque(/^il souhaite ajouter un tag existant pour un vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^séléctionne un tag existant de la liste$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^le tag séléctionné est bien associé au vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Etantdonnéque(/^il souhaite modifier un tag pour un vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^clique sur liste des tags$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quand(/^séléctionne l'option \(\-\) pour enlever un tag$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Alors(/^le tag séléctionné est bien dissocié du vitrail$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
