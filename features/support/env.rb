966c6993d913c0743b1d57f5f5cdee24a76df885
Shyked <remy.shyked@gmail.com>
2017-06-09 11:30:48 +0200

    Adding pry and fix app root URL

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,7 @@
 require 'capybara/cucumber'
+require 'pry'
 
 Capybara.configure do |config|
   config.default_driver = :selenium
-  config.app_host = 'http://localhost:8888/UTT/if05'
+  config.app_host = 'http://localhost:3000'
 end
